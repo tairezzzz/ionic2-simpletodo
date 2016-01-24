cd0631d7c8378805ce5736774b98290dff637733
tairezzzz <tairezzzz@gmail.com>
2016-01-25 00:00:24 +0200

    list and add item

--- app/pages/list/list.js
+++ app/pages/list/list.js
@@ -1,11 +1,22 @@
-import {Page} from 'ionic/ionic';
-
+import {Page, NavController} from 'ionic/ionic';
+import {AddItemPage} from '../add-item/add-item'
 
 @Page({
   templateUrl: 'build/pages/list/list.html'
 })
 export class ListPage {
-  constructor() {
+  constructor(nav:NavController) {
 
+    this.nav = nav;
+
+    this.items=[
+      {title:'hi', description:'whats up'},
+      {title:'hi2', description:'whats up'},
+      {title:'hi3', description:'whats up'},
+    ];
+  }
+
+  addItem(){
+    this.nav.push(AddItemPage,{ListPage:this});
   }
 }
